2ac9713357258d5c4bdc918c4b35cd2224ef1589
SOVEREIGN <isimemeni@gmail.com>
2020-11-22 18:29:56 -0500

    working on calculator functionality

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,44 +1,71 @@
 import React, {Component} from "react"
 import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col' 
 
 
 class App extends Component {
 
   state = {
-
+    firstNumber: 0,
+    secondNumber: "",
+    isCalculated: false,
+    isOperatorChosen: false,
+    result: 0
   }
 
+  result = () => {
+    this.setState({
+      result: this.state.firstNumber + this.state.secondNumber
+    })
+  }
+
+  addValue = () => {
+    this.setState({
+      firstNumber: this.state.firstNumber + parseInt(this.value)
+    })
+    console.log(this.state.firstNumber)
+  }
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+        [name]: value,
+        firstNumber: value
+    });
+    console.log(value);
+  };
+
+  showValue = () => {
+    console.log(parseInt(this.value))
+  }
 
   render(){
   return (
     <div>
       <Container>
-      <input></input>
+      <p>{this.state.firstNumber}</p>
       <Row>
-      <Button value="1">1</Button>
-      <Button value ="2">2</Button>
-      <Button value ="3">3</Button>
-      <Button value= "plus">+</Button>
+      <input type="button" value="1" onClick={this.handleInputChange}></input>
+      <button value="2">2</button>
+      <button value="3">3</button>
+      <button value= "+">+</button>
       </Row>
       <Row>
-      <Button value="4">4</Button>
-      <Button value="5">5</Button>
-      <Button value="6">6</Button>
-      <Button value="minus">-</Button>
+      <button value="4">4</button>
+      <button value="5">5</button>
+      <button value="6">6</button>
+      <button value="-">-</button>
       </Row>
       <Row>
-      <Button value="7">7</Button>
-      <Button value="8">8</Button>
-      <Button value="9">9</Button>
-      <Button value="multiply">X</Button>
+      <button value="7">7</button>
+      <button value="8">8</button>
+      <button value="9">9</button>
+      <button value="*">X</button>
       </Row>
       <Row>
-      <Button value="divide">/</Button>
-      <Button value="clear">CE</Button>
-      <Button value="solve">Enter</Button>
+      <button value="/">/</button>
+      <button value="clear">CE</button>
+      <button value="=">Enter</button>
       </Row>
       </Container>
     </div>
